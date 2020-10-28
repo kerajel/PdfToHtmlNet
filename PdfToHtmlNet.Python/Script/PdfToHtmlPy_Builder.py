81796b2c617accff044f82684a66a0843c45f762
kerajel <kerajel@gmail.com>
2020-10-28 17:22:09 +0300

    builder add

---
+++ PdfToHtmlNet.Python/Script/PdfToHtmlPy_Builder.py
@@ -0,0 +1,14 @@
+#This builts executable from the PdfToHtmlPy.py
+import subprocess
+from pathlib import Path
+
+pyScriptPath = str(Path.cwd()) + '\\PdfToHtmlPy.py'
+outputDir = str(Path(f"..\\Executable").resolve())
+cmd = f'pyinstaller --noconfirm --onefile --console "{pyScriptPath}" --distpath "{outputDir}"'
+out = subprocess.Popen(cmd, 
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT)
+stdout,stderr = out.communicate()
+print(stdout.decode("utf-8"))
+if (stderr != None):
+    print(stderr.decode("utf-8"))
